b847a30e43fb1dba6047e4da9aec4bf49fe363d9
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2025-02-03 23:59:36 +0400

    Tiny changes (It is late night)

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
@@ -20,7 +22,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeArticleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(modifier = Modifier.fillMaxSize()) {
                     ComposeArticleContent("Article", "Second")
                 }
             }
@@ -32,6 +34,12 @@ class MainActivity : ComponentActivity() {
 fun ComposeArticleContent(title: String, paragraph: String, modifier: Modifier = Modifier) {
     ComposeArticleTheme {
         Column {
+            // Image
+            Image(
+                painter = painterResource(R.drawable.bg_compose_background),
+                contentDescription = null
+            )
+
             // Title
             Text(
                 text = title,
