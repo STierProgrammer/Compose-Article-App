94259b7f287bee7e84f1d904b0e577935469d860
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2025-02-03 20:14:16 +0400

    Init

---
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeArticleTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    ComposeArticleContent("Article", "Second")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticleContent(title: String, paragraph: String, modifier: Modifier = Modifier) {
+    ComposeArticleTheme {
+        Column {
+            // Title
+            Text(
+                text = title,
+                fontSize = 100.sp
+            )
+
+            // Paragraph
+            Text(
+                text = paragraph,
+                fontSize = 50.sp
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ComposeArticlePreview() {
+    ComposeArticleTheme {
+        ComposeArticleContent("Article", "Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah")
+    }
+}
